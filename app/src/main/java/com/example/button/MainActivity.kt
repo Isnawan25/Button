33f7e2f32da9888ebcc1e0ac028e570d7d5bad3e
Isnawan25 <isnawanrifai@gmail.com>
2024-04-01 06:33:12 +0700

    Commit

---
+++ app/src/main/java/com/example/button/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.button
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.button.ui.theme.ButtonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ButtonTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Gambarrandom()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GambarRandom(modifier: Modifier = Modifier){
+    var hasil by remember {
+        mutableStateOf(1)
+    }
+
+    val gambarAcak = when(hasil){
+        1 -> R.drawable.hewan_1
+        2 -> R.drawable.hewan_2
+        3 -> R.drawable.hewan_3
+        4 -> R.drawable.hewan_4
+        else -> R.drawable.hewan_5
+
+    }
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(gambarAcak),
+            contentDescription = "1")
+
+        Button(onClick = { hasil = (1..5).random() }) {
+            Text(text = stringResource(id = R.string.buttonAcak))
+            
+        }
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun Gambarrandom(){
+    GambarRandom()
+}
