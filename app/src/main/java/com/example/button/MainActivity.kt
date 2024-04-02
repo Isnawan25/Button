9830021917d1dfeff13f704d404b5e2707eb0b62
Isnawan25 <isnawanrifai@gmail.com>
2024-04-02 14:29:53 +0700

    Aplikasi Sederhana

--- app/src/main/java/com/example/button/MainActivity.kt
+++ app/src/main/java/com/example/button/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,9 +20,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.button.ui.theme.ButtonTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,13 +42,66 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Background(pesan = "Ramadhan Penuh Berkah", pengirim ="Selamat Berbuka Puasa")
                     Gambarrandom()
                 }
             }
         }
     }
 }
+@Composable
+fun GreetingText(pesan: String, pengirim: String, modifier: Modifier = Modifier){
+    val fontbagus = FontFamily(Font(R.font.fontramadhan))
+    Column (verticalArrangement = Arrangement.Top)
+    {
+        Text(
+            text = pesan,
+            color = Color.White,
+            fontSize = 45.sp,
+            lineHeight = 45.sp,
+            textAlign = TextAlign.Center,
+            fontFamily = fontbagus
+            )
+    }
+    Column (verticalArrangement = Arrangement.Bottom,
+        modifier = Modifier.fillMaxSize())
+    {
+     Text(
+         text = pengirim,
+         color = Color.White,
+         fontSize = 40.sp,
+         lineHeight = 45.sp,
+         textAlign = TextAlign.Center,
+         modifier = Modifier.padding(0.dp),
+         fontFamily = fontbagus
+         )
+    }
+}
 
+@Composable
+fun Background(pesan: String, pengirim: String, modifier: Modifier = Modifier){
+    val Gambar = painterResource(R.drawable.backgroundramadhan,)
+    Box {
+        Image(
+            painter = Gambar,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            alpha = 0.5F,
+            contentScale = ContentScale.Crop
+        )
+        GreetingText(
+            pesan = pesan,
+            pengirim = pengirim,
+            modifier = Modifier.fillMaxSize())
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPriview(){
+    ButtonTheme { GreetingText(pesan = "Ramadhan Penuh Berkah", pengirim ="Selamat Berbuka Puasa")
+
+    }
+}
 @Composable
 fun GambarRandom(modifier: Modifier = Modifier){
     var hasil by remember {
@@ -47,11 +109,10 @@ fun GambarRandom(modifier: Modifier = Modifier){
     }
 
     val gambarAcak = when(hasil){
-        1 -> R.drawable.hewan_1
-        2 -> R.drawable.hewan_2
-        3 -> R.drawable.hewan_3
-        4 -> R.drawable.hewan_4
-        else -> R.drawable.hewan_5
+        1 -> R.drawable.mieayam
+        2 -> R.drawable.satepincuk
+        3 -> R.drawable.gorengan
+        else -> R.drawable.ayamgoreng
 
     }
     Column(
@@ -63,7 +124,7 @@ fun GambarRandom(modifier: Modifier = Modifier){
             painter = painterResource(gambarAcak),
             contentDescription = "1")
 
-        Button(onClick = { hasil = (1..5).random() }) {
+        Button(onClick = { hasil = (1..4).random() }) {
             Text(text = stringResource(id = R.string.buttonAcak))
             
         }
